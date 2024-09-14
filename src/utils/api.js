d08eaddb8195feac7250b506e64f6d0ee563cb72
AbdullahSharif <abdullah.29.umar@gmail.com>
2024-09-14 15:43:52 +0500

    updated the api url

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,4 @@
 // let url = "127.0.0.1:8000";
-const url = "doccomparegpt-backend:34832";
+const url = "doccomparegpt-backend.onrender.com";
 // let url = "192.168.100.240:8000";
 export default url;
